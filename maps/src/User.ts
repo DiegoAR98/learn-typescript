5b6cef5eba522d760974f424b57344f0df5c3548
DiegoAR98 <146890514+DiegoAR98@users.noreply.github.com>
2024-10-14 09:21:08 -0400

    maps

--- maps/src/User.ts
+++ maps/src/User.ts
@@ -1,18 +1,17 @@
 import { faker } from "@faker-js/faker";
 
 export class User {
- name: string;
- location: {
+  name: string;
+  location: {
     lat: number;
-    lng: Number;
- };
+    lng: number;
+  };
 
- constructor() {
+  constructor() {
     this.name = faker.name.firstName();
     this.location = {
-        lat: parseFloat (faker.address.latitude()),
-        lng: parseFloat (faker.address.longitude(),)
+      lat: parseFloat(faker.address.latitude()),
+      lng: parseFloat(faker.address.longitude()),
     };
- }
+  }
 }
-
--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,18 +1,10 @@
-/// <reference types="@types/google.maps" />
-
 import { User } from './User';
-import { Company } from './Company'
+import { Company } from './Company';
+import { CustomMap } from './CustomMap';
 
 const user = new User();
 const company = new Company();
+const customMap = new CustomMap('map');
 
-console.log(user);
-console.log(company);
-
-new google.maps.Map(document.getElementById('map') as HTMLElement, {
-    zoom: 1,
-    center: {
-        lat: 0,
-        lng: 0,
-    }
-});
+customMap.addMarker(user);
+customMap.addMarker(company);
